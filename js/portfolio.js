be3a92063e8e8c0aa55494d5873d09ecbed943be
AndriiMshk <90900795+AndriiMshk@users.noreply.github.com>
2022-01-29 16:44:50 +0200

    2

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,21 +1,18 @@
 const seasonBtns = document.querySelectorAll(".season_btn");
 const seasonImg = document.querySelectorAll(".portfolio_item");
-const seasenImgs = document.querySelector('.portfolio_items')
+const seasenImgs = document.querySelector(".portfolio_items");
 
 const handleChangeSeason = (season) => {
-    seasonBtns.forEach((button) => {
-      seasenImgs.classList.add('open')
-      if (button.dataset.season === season) {
-        button.classList.add("active");
-        seasenImgs
-        seasonImg.forEach(
-          (img, i) =>
-            (img.src = `./img/seasons/${season}/${
-              i + 1
-            }.jpg`)
-        );
-      } else {
-        button.classList.remove("active");
-      }
-    });
-  };
+  seasonBtns.forEach((button) => {
+    seasenImgs.classList.add("open");
+    if (button.dataset.season === season) {
+      button.classList.add("active");
+      seasenImgs;
+      seasonImg.forEach(
+        (img, i) => (img.src = `./img/seasons/${season}/${i + 1}.jpg`)
+      );
+    } else {
+      button.classList.remove("active");
+    }
+  });
+};
--- js/themes.js
+++ js/themes.js
@@ -1,6 +1,6 @@
 const switchThemeBtn = document.querySelector("#switch-theme");
 
 switchThemeBtn.addEventListener("click", () => {
-    const isLight = switchThemeBtn.checked ? "add" : "remove";
-    document.body.classList[isLight]("light");
-  });
+  const isLight = switchThemeBtn.checked ? "add" : "remove";
+  document.body.classList[isLight]("light");
+});
