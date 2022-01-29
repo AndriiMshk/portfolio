de00acc7beeebfc77fcc342e1b998886626f8b35
AndriiMshk <90900795+AndriiMshk@users.noreply.github.com>
2022-01-29 23:50:59 +0200

    almost done

--- js/language.js
+++ js/language.js
@@ -1,30 +1,49 @@
-import i18Obj from './translate.js';
+import i18Obj from "./translate.js";
 
 const wordTranslate = document.querySelectorAll("[data-i18]");
 const langEn = document.querySelector(".lang-en");
 const langRu = document.querySelector(".lang-ru");
 
 langEn.addEventListener("click", function () {
-    langEn.classList.add('active')
-    langRu.classList.remove('active')
-    wordTranslate.forEach((element) => {
-      const newText = i18Obj["en"][element.dataset.i18];
-      element.textContent = newText;
-      if (element.placeholder) {
-        element.placeholder = newText;
-      }
-    });
+  localStorage.setItem("language", "en");
+  langEn.classList.add("active");
+  langRu.classList.remove("active");
+  wordTranslate.forEach((element) => {
+    const newText = i18Obj["en"][element.dataset.i18];
+    element.textContent = newText;
+    if (element.placeholder) {
+      element.placeholder = newText;
+    }
   });
-  
-  langRu.addEventListener("click", function () {
-    langRu.classList.add('active')
-    langEn.classList.remove('active')
-    wordTranslate.forEach((element) => {
-      const newText = i18Obj["ru"][element.dataset.i18];
-      element.textContent = newText;
-      if (element.placeholder) {
-        element.placeholder = newText;
-      }
-    });
-  });
+});
 
+langRu.addEventListener("click", function () {
+  localStorage.setItem("language", "ru");
+  langRu.classList.add("active");
+  langEn.classList.remove("active");
+  wordTranslate.forEach((element) => {
+    const newText = i18Obj["ru"][element.dataset.i18];
+    element.textContent = newText;
+    if (element.placeholder) {
+      element.placeholder = newText;
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const language = localStorage.getItem("language") || "en";
+  if (language === "en") {
+    langEn.classList.add("active");
+    langRu.classList.remove("active");
+  } else {
+    langRu.classList.add("active");
+    langEn.classList.remove("active");
+  }
+  wordTranslate.forEach((element) => {
+    const newText = i18Obj[language][element.dataset.i18];
+    element.textContent = newText;
+    if (element.placeholder) {
+      element.placeholder = newText;
+    }
+  });
+});
--- js/themes.js
+++ js/themes.js
@@ -1,6 +1,15 @@
 const switchThemeBtn = document.querySelector("#switch-theme");
 
 switchThemeBtn.addEventListener("click", () => {
+  localStorage.setItem('theme', switchThemeBtn.checked ? 'light' : 'dark')
   const isLight = switchThemeBtn.checked ? "add" : "remove";
   document.body.classList[isLight]("light");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const theme = localStorage.getItem('theme');
+  if(theme === 'light'){
+    switchThemeBtn.checked = true;
+    document.body.classList.add("light");
+  }
+})
