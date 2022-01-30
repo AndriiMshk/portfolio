94b36dbdd849ef4311980a2bd793047ca650ee8e
AndriiMshk <90900795+AndriiMshk@users.noreply.github.com>
2022-01-30 22:25:11 +0200

    1

--- js/click.js
+++ js/click.js
@@ -1,7 +1,6 @@
 const button = document.querySelectorAll("button")
 console.log(button);
 
-
 button.forEach((button) => {
 button.addEventListener('click', function (e) {
     console.log('click');
